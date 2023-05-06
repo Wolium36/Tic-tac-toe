08a99954fa9fa6b4a7850b95060d8d5f76d0a9fc
Wolium36 <130088065+Wolium36@users.noreply.github.com>
2023-05-06 15:29:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,88 @@
-var tura=0;
-var oczka=new Array('-','-','-','-','-','-','-','-','-');
+var round=0;
+var mesh=new Array('-','-','-','-','-','-','-','-','-');
 
-var wygraneX=0;
-var wygraneY=0;
-
-document.getElementById("wygraneX").innerHTML=wygraneX;
-document.getElementById("wygraneY").innerHTML=wygraneY;
+var winsX=0;
+var winsO=0;
 
 function reset(){
-  document.getElementById("wygraneX").innerHTML=wygraneX;
-  document.getElementById("wygraneY").innerHTML=wygraneY; 
-
-  tura=0;
+  document.getElementById("after").innerHTML="";
+  round=0;
 
   for(var i=0; i<9; i++){
-    oczka[i]='-'
-    document.getElementById("k"+(i+1)).innerHTML = "";
+    mesh[i]='-';
+    document.getElementById("s"+(i)).innerHTML = "";
   }
 }
 
-function sprawdzWygrana(t){
-  if(oczka[0]==oczka[1] && oczka[1]==oczka[2] && oczka[0]!='-'){
-    if(oczka[0]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[0]=='o'){
-      wygraneY++;
-      reset();
+const btn0 = document.getElementById("s0");
+btn0.addEventListener("click", function(){checkMesh(0,"s0")});
+const btn1 = document.getElementById("s1");
+btn1.addEventListener("click", function(){checkMesh(1,"s1")});
+const btn2 = document.getElementById("s2");
+btn2.addEventListener("click", function(){checkMesh(2,"s2")});
+const btn3 = document.getElementById("s3");
+btn3.addEventListener("click", function(){checkMesh(3,"s3")});
+const btn4 = document.getElementById("s4");
+btn4.addEventListener("click", function(){checkMesh(4,"s4")});
+const btn5 = document.getElementById("s5");
+btn5.addEventListener("click", function(){checkMesh(5,"s5")});
+const btn6 = document.getElementById("s6");
+btn6.addEventListener("click", function(){checkMesh(6,"s6")});
+const btn7 = document.getElementById("s7");
+btn7.addEventListener("click", function(){checkMesh(7,"s7")});
+const btn8 = document.getElementById("s8");
+btn8.addEventListener("click", function(){checkMesh(8,"s8")});
+
+
+function afterGame(){
+  document.getElementById("after").innerHTML='<div class="background">'+
+                                              '<button id="newGame">new game</button>'
+                                             '</div>';
+  
+  const btnNewGame = document.getElementById("newGame");
+  btnNewGame.addEventListener("click", function(){reset()});
+
+}
+
+function checkWin(n){
+  if((mesh[0]==mesh[1] && mesh[1]==mesh[2] && mesh[0]!='-') ||
+     (mesh[3]==mesh[4] && mesh[4]==mesh[5] && mesh[3]!='-') ||
+     (mesh[6]==mesh[7] && mesh[7]==mesh[8] && mesh[6]!='-') ||
+     (mesh[0]==mesh[3] && mesh[3]==mesh[6] && mesh[0]!='-') ||
+     (mesh[1]==mesh[4] && mesh[4]==mesh[7] && mesh[1]!='-') ||
+     (mesh[2]==mesh[5] && mesh[5]==mesh[8] && mesh[2]!='-') ||
+     (mesh[0]==mesh[4] && mesh[4]==mesh[8] && mesh[0]!='-') ||
+     (mesh[2]==mesh[4] && mesh[4]==mesh[6] && mesh[2]!='-')){
+    if(mesh[n]=='x'){
+      winsX++;
+      if(winsX>0){
+        document.getElementById("winsX").innerHTML=winsX; 
+      }
+      afterGame();
+    }else if(mesh[n]=='o'){
+      winsO++;
+      if(winsO>0){
+        document.getElementById("winsO").innerHTML=winsO; 
+      }
     }
-  }else if(oczka[3]==oczka[4] && oczka[4]==oczka[5] && oczka[3]!='-'){
-    if(oczka[3]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[3]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[6]==oczka[7] && oczka[7]==oczka[8] && oczka[6]!='-'){
-    if(oczka[6]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[6]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[0]==oczka[3] && oczka[3]==oczka[6] && oczka[0]!='-'){
-    if(oczka[0]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[0]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[1]==oczka[4] && oczka[4]==oczka[7] && oczka[1]!='-'){
-    if(oczka[1]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[1]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[2]==oczka[5] && oczka[5]==oczka[8] && oczka[2]!='-'){
-    if(oczka[2]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[2]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[0]==oczka[4] && oczka[4]==oczka[8] && oczka[0]!='-'){
-    if(oczka[0]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[0]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(oczka[2]==oczka[4] && oczka[4]==oczka[6] && oczka[2]!='-'){
-    if(oczka[2]=='x'){
-      wygraneX++;
-      reset();
-    }else if(oczka[2]=='o'){
-      wygraneY++;
-      reset();
-    }
-  }else if(t==9){
-    reset();
+    afterGame();
+  }else if(round==9){
+    afterGame()
   }
 }
 
-function sprawdzOczko(n,k){
-  if(tura%2==0){
-    document.getElementById(k).innerHTML = '<div class="krzyzL"></div><div class="krzyzR"></div>';
-    oczka[n]='x';
-  }else{
-    document.getElementById(k).innerHTML = '<div class="kolo"></div>';
-    oczka[n]='o';
+function checkMesh(n,s){
+  if(mesh[n]=='-'){
+    if(round%2==0){
+      document.getElementById(s).innerHTML = '<div class="crossL"></div><div class="crossR"></div>';
+      mesh[n]='x';
+    }else{
+      document.getElementById(s).innerHTML = '<div class="circle"></div>';
+      mesh[n]='o';
+    }
+    round++;
+    checkWin(n);
   }
-  tura++;
-  sprawdzWygrana(tura);
 }
 
-function gra(kwadrat){
-  if(kwadrat=="k1"){
-    if(oczka[0]=='-'){
-      sprawdzOczko(0,"k1");
-    }
-  }  
-  if(kwadrat=="k2"){
-    if(oczka[1]=='-'){
-      sprawdzOczko(1,"k2");
-    }
-  }  
-  if(kwadrat=="k3"){
-    if(oczka[2]=='-'){
-      sprawdzOczko(2,"k3");
-    }
-  }  
-  if(kwadrat=="k4"){
-    if(oczka[3]=='-'){
-      sprawdzOczko(3,"k4");
-    }
-  }  
-  if(kwadrat=="k5"){
-    if(oczka[4]=='-'){
-      sprawdzOczko(4,"k5");
-    }
-  }  
-  if(kwadrat=="k6"){
-    if(oczka[5]=='-'){
-      sprawdzOczko(5,"k6");
-    }
-  }  
-  if(kwadrat=="k7"){
-    if(oczka[6]=='-'){
-      sprawdzOczko(6,"k7");
-    }
-  }  
-  if(kwadrat=="k8"){
-    if(oczka[7]=='-'){
-      sprawdzOczko(7,"k8");
-    }
-  }  
-  if(kwadrat=="k9"){
-    if(oczka[8]=='-'){
-      sprawdzOczko(8,"k9");
-    }
-  }  
-}
-
-const btn1 = document.getElementById("k1");
-btn1.addEventListener("click", function(){gra("k1")});
-const btn2 = document.getElementById("k2");
-btn2.addEventListener("click", function(){gra("k2")});
-const btn3 = document.getElementById("k3");
-btn3.addEventListener("click", function(){gra("k3")});
-const btn4 = document.getElementById("k4");
-btn4.addEventListener("click", function(){gra("k4")});
-const btn5 = document.getElementById("k5");
-btn5.addEventListener("click", function(){gra("k5")});
-const btn6 = document.getElementById("k6");
-btn6.addEventListener("click", function(){gra("k6")});
-const btn7 = document.getElementById("k7");
-btn7.addEventListener("click", function(){gra("k7")});
-const btn8 = document.getElementById("k8");
-btn8.addEventListener("click", function(){gra("k8")});
-const btn9 = document.getElementById("k9");
-btn9.addEventListener("click", function(){gra("k9")});
 
